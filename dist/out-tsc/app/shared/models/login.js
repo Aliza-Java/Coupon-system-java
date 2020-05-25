39656efde8e90ad16fbad58b7c91beb99bdf780e
SpringIntoJava <springintojava@gmail.com>
2020-05-25 13:24:00 +0300

    Fixed CORS

--- dist/out-tsc/app/shared/models/login.js
+++
@@ -1,8 +0,0 @@
-export class Login {
-    constructor(username, password, type) {
-        this.username = username;
-        this.password = password;
-        this.type = type;
-    }
-}
-//# sourceMappingURL=login.js.map
--- dist/out-tsc/app/shared/models/user.js
+++
@@ -1,7 +0,0 @@
-export class User {
-    constructor(id, type) {
-        this.id = id;
-        this.type = type;
-    }
-}
-//# sourceMappingURL=user.js.map
